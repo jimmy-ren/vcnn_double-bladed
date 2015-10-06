61013919bbf709c2931834efeda3b476f7d7d257
Jimmy Ren <jimmy.sj.ren@gmail.com>
2015-10-06 18:33:20 +0800

    Simplify the pipeline with mask.

--- pipeline/op_train_pipe_with_mask.m
+++ pipeline/op_train_pipe_with_mask.m
@@ -1,19 +1,5 @@
-function op_train_pipe_with_mask(in, mask, gt_out)
-    global config mem;
-    
-    % forward pass
-    config.misc.training = 1;
-    mem.GT_output = gt_out;
-    config.pipeline_forward{1}(in);
-    config.pipeline_forward{2}(mask);
-    for m = 3:length(config.pipeline_forward)
-        config.pipeline_forward{m}();
-    end
-    config.misc.current_layer = config.misc.current_layer - 1;
-    % backprop
-    for m = 1:length(config.pipeline_backprop)
-        config.pipeline_backprop{m}();
-    end
-    config.misc.current_layer = 1;
+function op_train_pipe_with_mask(in, mask_li, gt_out)
+    set_mask_list(mask_li);
+    op_train_pipe(in, gt_out);
 end
 
