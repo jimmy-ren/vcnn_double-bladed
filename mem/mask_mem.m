00b6e3374c6e0cabcf15b32a6cab0ee4f62a5182
Jimmy Ren <jimmy.sj.ren@gmail.com>
2015-10-06 18:32:44 +0800

    Remove the unnecessary memory init.

--- mem/mask_mem.m
+++ mem/mask_mem.m
@@ -2,8 +2,7 @@ function mask_mem()
     global config mem;
     mem.mask_inputs{1} = config.NEW_MEM(zeros(config.kernel_size(1, 1)*config.kernel_size(1, 2)*config.chs, ...
                     (config.input_size(1)-config.kernel_size(1, 1)+1)*(config.input_size(2)-config.kernel_size(1, 2)+1)*config.batch_size));
-	mem.mask_activations{1} = config.NEW_MEM(zeros(config.feature_map_sizes{1}(3), config.feature_map_sizes{1}(1)*config.feature_map_sizes{1}(2)));
-    %mem.mask_intermediate{1} = config.NEW_MEM(zeros(size(config.weights{1},1), size(config.weights{1},2), size(mem.layer_inputs{1},2)));
+	%mem.mask_activations{1} = config.NEW_MEM(zeros(config.feature_map_sizes{1}(3), config.feature_map_sizes{1}(1)*config.feature_map_sizes{1}(2)));    
     mem.mask_fun = @(A,B) A*B;
 end
 
